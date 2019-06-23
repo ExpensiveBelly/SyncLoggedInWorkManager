5c8a1a8242c5c15acb69ffd34d28e310f3c56ee6
ExpensiveBelly <lucas_caro_barriga@hotmail.com>
2019-06-23 21:08:26 +0100

    Initial project set up (Login screen)

---
+++ app/src/main/java/com/expensivebelly/syncloggedinworkmanager/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.expensivebelly.syncloggedinworkmanager.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
